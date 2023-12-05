802a53100697745fcf2838baf6b34c3ba71669f0
Soorya U <sooryau7@gmail.com>
2023-12-05 14:29:52 +0530

    Added Form Logic

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 import "../styles/Login.css";
 
 export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function postData(username, password) {
+    console.log("Username", username);
+    console.log("Password", password);
+  }
+
   return (
     <>
       <main className="flex justify-center content-center flex-col gap-[3rem] flex-wrap">
@@ -10,25 +20,36 @@ export const Login = () => {
 
         <form
           action="post"
-          className="w-[35vw] flex justify-center content-center flex-col gap-[3rem] flex-wrap rounded-[10px]"
+          className="flex justify-center content-center flex-col gap-[3rem] flex-wrap rounded-[10px]"
         >
-          <div className="flex justify-center content-center gap-[1.2rem] flex-wrap">
+          <div className="px-[1.5rem] flex justify-center content-center gap-[1.2rem] flex-wrap">
             <label className="text-[1.2rem] text-[#FF007A]" htmlFor="username">
               {" "}
               Username:
             </label>
-            <input type="text" name="username" />
+            <input
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+              type="text"
+              name="username"
+            />
           </div>
-          <div className="flex justify-center content-center gap-[1.2rem] flex-wrap">
+          <div className="px-[1.5rem] flex justify-center content-center gap-[1.2rem] flex-wrap">
             <label className="text-[1.2rem] text-[#FF007A]" htmlFor="password">
               {" "}
               Password:
             </label>
-            <input type="password" name="password" />
+            <input
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              type="password"
+              name="password"
+            />
           </div>
           <div className="button-holder flex justify-center flex-col content-center gap-[1.5rem] flex-wrap">
             <button
               type="submit"
+              onClick={postData(username, password)}
               className="font-['Cairo'] py-[0.5rem] px-[1.2rem] text-[1.2rem] text-[#FF007A]"
             >
               LOGIN
