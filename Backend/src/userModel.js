d46e3984259c38a89f6608fdcb2a6f64d832c3b3
Puneeth <pyd773@gmail.com>
2023-12-05 20:18:01 +0530

    Fixing issues

--- Backend/src/userModel.js
+++ Backend/src/userModel.js
@@ -4,12 +4,7 @@ require("dotenv").config();
 
 const Mongo_connect = async (app) => {
   try {
-<<<<<<< HEAD
     const uri = process.env.MONGO_URI;
-=======
-    const uri =
-      "####################################";
->>>>>>> 97039a310ac627b16b7edf0d5d22b8a105e7b0c1
     await mongoose.connect(uri);
     console.log("Connected to MongoDB...");
   } catch (error) {
