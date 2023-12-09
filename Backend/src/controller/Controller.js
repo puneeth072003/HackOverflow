04e41048f1ffd61bd23574d2388e08cd2f9600c9
Puneeth <pyd773@gmail.com>
2023-12-09 21:54:19 +0530

    OAuth integration done

--- Backend/src/controller/Controller.js
+++ Backend/src/controller/Controller.js
@@ -1,5 +1,33 @@
+require("dotenv").config();
+
 const getHome = async (req, res) => {
   res.send("Hi huddle here");
 };
 
-module.exports = { getHome };
+const fetchAccessToken = async (code) => {
+  try {
+    const response = await fetch("https://oauth2.googleapis.com/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: "http://localhost:3500/api/v1/login/callback",
+        client_id: process.env.CALENDER_CLIENT_ID,
+        client_secret: process.env.CALENDER_CLIENT_SECRET,
+      }).toString(),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching Google Calendar access token:", error);
+  }
+};
+
+module.exports = { getHome, fetchAccessToken };
--- Frontend/src/components/Login/index.jsx
+++ Frontend/src/components/Login/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import axios from "axios";
 import "./style.css";
 
 export const Login = () => {
@@ -17,7 +18,21 @@ export const Login = () => {
     setAuth(true);
     return;
   }
+  // ################################################
+  const url = "http://localhost:3500/api/v1/login";
 
+  const handleLogin = async () => {
+    try {
+      const response = await axios.get(url);
+      if (response.data.redirectUrl) {
+        window.location.href = response.data.redirectUrl;
+      }
+    } catch (error) {
+      console.error("Error in login:", error);
+    }
+  };
+
+  // ################################################
   useEffect(() => {
     auth && navigate("/home");
   }, [auth, navigate]);
@@ -25,7 +40,11 @@ export const Login = () => {
   return (
     <>
       <main className="flex justify-center content-center flex-col gap-[3rem] flex-wrap">
-        <img src={logo} alt="" className="w-[15rem] justify-self-center self-center" />
+        <img
+          src={logo}
+          alt=""
+          className="w-[15rem] justify-self-center self-center"
+        />
         <h1 className="font-['Cairo'] text-center text-[2rem] font-bold text-[#FF007A]">
           LOGIN TO CONTINUE
         </h1>
@@ -44,6 +63,7 @@ export const Login = () => {
               onChange={(event) => setUsername(event.target.value)}
               type="text"
               name="username"
+              id="username"
             />
           </div>
           <div className="px-[1.5rem] flex justify-center content-center gap-[1.2rem] flex-wrap">
@@ -55,6 +75,7 @@ export const Login = () => {
               onChange={(event) => setPassword(event.target.value)}
               type="password"
               name="password"
+              id="password"
             />
           </div>
           <div className="button-holder flex justify-center flex-col content-center gap-[1.5rem] flex-wrap">
@@ -67,6 +88,7 @@ export const Login = () => {
             </button>
             <button
               data-function="google-auth"
+              onClick={handleLogin}
               className="font-['Cairo'] py-[0.5rem] px-[1.2rem] text-[1rem] text-[#ccc]"
             >
               LOGIN WITH GOOGLE
